df958a339ded1d0246f1f54703e25809a724251c
Thales <thalespizza@gmail.com>
2017-02-04 01:20:22 -0200

    App, Header, Order e Inventory components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,68 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import './css/style.css';
+/*
+    App 
+*/
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header />    
+                </div>
+                <Order />
+                <Inventory />    
+            </div>
+        )    
+    }    
+});
+/*
+    Header
+*/
+var Header = React.createClass({
+    render: function() {
+        return (
+            <p>Header</p>
+        )
+    }
+    
+});
+/*
+    Order
+*/
+var Order = React.createClass({
+    render: function() {
+        return (
+            <p>Order</p>
+        )
+    }
+    
+});
+/*
+    Inventory
+*/
+var Inventory = React.createClass({
+    render: function() {
+        return (
+            <p>Inventory</p>
+        )
+    }
+    
+});
 /*
     Store Picker
-    This will let us make <StorePicker/>
 */
-
 var StorePicker = React.createClass({
     render: function() {
         return (
-            <p>hello</p>
+            <form className="store-selector">
+                <h2>Please Enter A Store</h2>
+                <input type="text" ref="storeId" />
+                <input type="submit" value="Submit" />
+            </form>
         )    
     }    
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
